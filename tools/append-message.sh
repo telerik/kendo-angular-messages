7b10c22f40a3cbc3f1b54d75c5b1240d4eeaf6e5
dtopalov <dimiter.topalov79@gmail.com>
2021-06-21 16:34:39 +0300

    chore: address comments

--- tools/append-message.sh
+++ tools/append-message.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env sh
 
 COMPONENT=$1
+SOURCE=$2
 
-if [ ! $# -eq 1 ]; then
+if [ ! $# -eq 2 ]; then
   echo "
 Appends the content of a new message(s) file to all existing locale-specific files.
 
 Use this tool when creating new message(s) for an existing component.
 
-1. Create a 'new-message.yml' file containing the new message in the root folder.
+1. Create a file containing the new message(s) and pass the path to it as a second argument to the command.
    Start and end the file with an empty row, and provide the same indentation for the
    new message(s) and their description(s) as in the existing files.
-2. Run 'npm run append-message <component>'
+2. Run 'npm run append-message <component> <path-to-the-new-file>'
 3. Commit the updated files from 'messages/component'
+4. Cleanup and make sure that the new messages file is not committed
   "
   exit
 fi
 
-SOURCE="new-message.yml"
 if [ ! -f "$SOURCE" ]; then
   echo "Input file '$SOURCE' does not exist"
   exit 1
@@ -26,5 +27,3 @@ fi
 for file in ./messages/$COMPONENT/*.yml; do
     cat $SOURCE >> "$file"
 done
-
-rm $SOURCE
